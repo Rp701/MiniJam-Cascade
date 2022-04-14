54bd8dee836c3aaa9f9086332d6dff81048b721e
PlayToTroll <playtotroll@gmx.ch>
2022-04-14 22:44:53 +0200

    Walking and Jump

--- MiniJam-Cascade/Assets/Scripts/PlayerMovement.cs
+++ MiniJam-Cascade/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed = 10;
+    public float movementSpeed = 15f;
     Rigidbody rb;
+    public float jumpHeight = 7.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +17,18 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (isGrounded() && Input.GetButton("Jump"))
+        {
+            Debug.Log(transform.position.y);
+            rb.velocity = Vector3.up * jumpHeight;
+        }
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         rb.MovePosition(transform.position + input * Time.deltaTime * movementSpeed);
+    }
+
+    public bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1.1f);
     }
 }
