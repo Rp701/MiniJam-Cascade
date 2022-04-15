a1da9d66c9e9ec8ccc9ba699ccfaebd60820c550
PlayToTroll <playtotroll@gmx.ch>
2022-04-15 18:13:56 +0200

    push

--- MiniJam-Cascade/Assets/Scripts/MouseLook.cs
+++ MiniJam-Cascade/Assets/Scripts/MouseLook.cs
@@ -7,6 +7,8 @@ public class MouseLook : MonoBehaviour
     float mouseSensitivity = 100f;
 
     public Transform playerBody;
+
+    float xRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,9 @@ public class MouseLook : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         playerBody.Rotate(Vector3.up * mouseX);
     }
