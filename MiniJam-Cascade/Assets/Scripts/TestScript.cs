0a4d72b61921c1d11c2e99e0b093ba010c8baedf
Alejandro Zamora <fordtauroslx@gmail.com>
2022-04-15 09:22:06 -0700

    test scene
    added new scene and small script

---
+++ MiniJam-Cascade/Assets/Scripts/TestScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("this is a starting screen");        
+    }
+}
