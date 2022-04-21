fa822ed45b039e8347801019a69a0a7342254583
PlayToTroll <playtotroll@gmx.ch>
2022-04-22 01:59:43 +0200

    Scenefade Black Car win

---
+++ MiniJam-Cascade/Assets/Scripts/CarWin.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class CarWin : MonoBehaviour
+{
+    Inventory inventoryScript;
+    GameObject Canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        Canvas = gameObject.transform.Find("Canvas").gameObject;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (inventoryScript.acquiredCarKeys)
+        {
+            Canvas.transform.Find("PressFToWin").gameObject.SetActive(true);
+        } else
+        {
+            Canvas.transform.Find("GetCarKey").gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (inventoryScript.acquiredCarKeys && Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("You win");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (inventoryScript.acquiredCarKeys)
+        {
+            Canvas.transform.Find("PressFToWin").gameObject.SetActive(false);
+        }
+        else
+        {
+            Canvas.transform.Find("GetCarKey").gameObject.SetActive(false);
+        }
+    }
+}
