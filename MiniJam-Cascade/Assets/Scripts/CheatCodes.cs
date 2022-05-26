83cdf0e4476328baeaaf10a5702823e2fd03a939
Alejandro Zamora <fordtauroslx@gmail.com>
2022-05-26 01:55:26 -0700

    Added Performance UI Stats
    Added some new performance statistics available through the UI.
    
    Tracks the system specs and also displays some FPS counter
    
    There are now two scenes, a Post Processing On scene and one that has it off. Switch between them by typing SWITCH in game

--- MiniJam-Cascade/Assets/Scripts/CheatCodes.cs
+++ MiniJam-Cascade/Assets/Scripts/CheatCodes.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class CheatCodes : MonoBehaviour
 {
+    [Header ("Cheat Code Name:")]
+    public string type_name_on_left;
+
+    [Header ("ItemsBeingMessedWith")]    
+    public GameObject fpsCounterText;
+    public string sceneToLoad;
+
+    [Space]
+
     [Tooltip("The Key that you press to continue/start the cheat")]
-    public KeyCode[] CheatCode;
+    public KeyCode[] CheatCodeLength;
     [Tooltip("Just attach the gameobject where the CheatCodes script is on")]
     public UnityEvent CheatEvent;
     [Tooltip("The amount of time the player has in between inputs to continue the code.")]
@@ -28,7 +38,7 @@ public class CheatCodes : MonoBehaviour
         if (Input.anyKeyDown)
         {
             //If its the next Key in the sequence, move onto the next
-            if (Input.GetKeyDown(CheatCode[_index]))
+            if (Input.GetKeyDown(CheatCodeLength[_index]))
             {
                 //Move onto the next one
                 _index++;
@@ -41,7 +51,7 @@ public class CheatCodes : MonoBehaviour
             }
         }
 
-        if (_index == CheatCode.Length)
+        if (_index == CheatCodeLength.Length)
         {
             ResetCheatInput();
             CheatEvent.Invoke();
@@ -56,10 +66,25 @@ public class CheatCodes : MonoBehaviour
     }
 
     //Here's where you would put whatever cheatcode you want!
-    public void Cheat()
+    public void IncreaseSpeed()
     {
         Debug.Log("CHEAT ACTIVATED");
         //increase the player's run speed by 3x
         GameObject.Find("FirstPersonPlayer").GetComponent<PlayerMovement>().runSpeed *= 3;
+    }
+
+    public void Enable_fpsCounter()
+    {
+        fpsCounterText.SetActive(true);
+    }
+
+    public void Disable_fpsCounter()
+    {
+        fpsCounterText.SetActive(false);
+    }
+
+    public void SwitchScenes()
+    {
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
---
+++ MiniJam-Cascade/Assets/Scripts/Dev Tools/Graphics_fpsCounter.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Graphics_fpsCounter : MonoBehaviour
+{
+    public Text uiText;
+
+    private int lastFrameIndex;
+    private float[] frameDeltaTimeArray;
+
+    private void Awake()
+    {
+        frameDeltaTimeArray = new float[50];
+    }
+
+    private void Update()
+    {
+        frameDeltaTimeArray[lastFrameIndex] = Time.unscaledDeltaTime;
+        lastFrameIndex = (lastFrameIndex + 1) % frameDeltaTimeArray.Length;
+
+        uiText.text = Mathf.RoundToInt(CalculateFPS()).ToString();
+    }
+
+    private float CalculateFPS()
+    {
+        float total = 0f;
+        foreach (float deltaTime in frameDeltaTimeArray)
+        {
+            total += deltaTime;
+        }
+        return frameDeltaTimeArray.Length / total;
+    }
+}
